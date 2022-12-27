16fb69010bdfd8176e79c9d5c69a3c14bb25ec39
afrokayie <oluwolekayode61@gmail.com>
2022-12-27 10:00:43 +0000

    version1

---
+++ app.js
@@ -0,0 +1,11 @@
+// traversing the dom
+
+
+const btns = document.querySelectorAll(".question-btn");
+
+btns.forEach(function (btn) {
+    btn.addEventListener("click", function(e){
+        const question = e.currentTarget.parentElement.parentElement;
+        question.classList.toggle("show-text");
+    });
+});
